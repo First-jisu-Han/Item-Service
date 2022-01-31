f08fefd7c46b0c1d4c36f386e6eef83c02cbc1c0
Jisu Han <weblite@naver.com>
2022-01-31 18:41:15 +0900

    상품 상세 페이지 동적 변환

--- src/main/java/hello/itemservice/web/BasicItemController.java
+++ src/main/java/hello/itemservice/web/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -35,5 +36,14 @@ public class BasicItemController {
         itemRepository.save(new Item("TestA",10000,10));
         itemRepository.save(new Item("TestB",20000,20));
     }
+
+    // 상품 상세
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model){
+        Item item=itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/item";
+    }
+
 
 }
