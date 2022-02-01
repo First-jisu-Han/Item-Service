dd98c2c46d7ecc7dd61256df9d073b7eb820f420
Jisu Han <weblite@naver.com>
2022-02-01 12:02:52 +0900

    상품 수정 처리 - 리다이렉트 사용

--- src/main/java/hello/itemservice/web/BasicItemController.java
+++ src/main/java/hello/itemservice/web/BasicItemController.java
@@ -64,7 +64,6 @@ public class BasicItemController {
 
         return "basic/item";
     }
-
     @PostMapping("/add")
     public String addItemV2(@ModelAttribute("item") Item item){
 
@@ -73,8 +72,20 @@ public class BasicItemController {
         return "basic/item";
     }
 
-
-
-
-
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/editForm";
     }
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item){
+        itemRepository.update(itemId,item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+
+
+
+
+}
